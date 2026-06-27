42e5f6f59fa0401e141a911d67fdde873f76ebc1
장주현 <jangjuhyeon@jangjuhyeon-ui-MacBookAir.local>
2026-06-27 20:33:37 +0900

    conftest update

--- conftest.py
+++ conftest.py
@@ -1,12 +1,14 @@
 #conftest.py
 import pytest
 from selenium import webdriver
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 @pytest.fixture
 def driver():
     driver = webdriver.Chrome()
     driver.get("https://automationexercise.com")
-    time.sleep(2)
+    WebDriverWait(driver, 10).until( EC.presence_of_element_located((By.TAG_NAME, "body")))
     yield driver
     driver.quit()
