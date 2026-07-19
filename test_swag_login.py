33b325fe73ce287b6e000c7c6d30b1d4ab5a9c3e
장주현 <jangjuhyeon@jangjuhyeon-ui-MacBookAir.local>
2026-07-19 21:09:47 +0900

    Add SauceDemo login test

---
+++ test_swag_login.py
@@ -0,0 +1,23 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+def test_login():
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+
+    username = driver.find_element(By.ID, "user-name")
+    username.send_keys("standard_user")
+    
+    print(username.get_attribute("value"))
+    
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    login_btn = driver.find_element(By.ID, "login-button")
+    driver.execute_script("arguments[0].click();", login_btn)
+
+    time.sleep(3)
+
+    assert "inventory.html" in driver.current_url
+
+
